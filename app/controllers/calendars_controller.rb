541ecfcf21ca7c5ece6bc9bac946cafaa99a7d4d
kazuma516 <kizuma.9516@gmail.com>
2022-10-13 21:04:33 +0900

    Issue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -7,15 +7,16 @@ class CalendarsController < ApplicationController
   end
 
   # 予定の保存
-  def create
+  def create    
     Plan.create(plan_params)
+    binding.pry
     redirect_to action: :index
   end
 
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def getWeek
